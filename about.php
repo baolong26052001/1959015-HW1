dd6bf62cb3287a880f038ad5a2b9f9b81fcc044b
baolong26052001 <80016176+baolong26052001@users.noreply.github.com>
2021-04-29 09:54:33 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,22 @@
+
+<?php
+include 'includes/header.php';
+ ?>
+<div class="container">
+  <div class="row">
+    <hr>
+    <h2>About me</h2>
+    <hr>
+    <div class="economy col-2">
+      <img src="images/ava.png" alt="">
+      <div class="right">
+        <h1>Hi! I am Long!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium necessitatibus, quam ab aliquam labore aperiam, omnis voluptas ipsa veniam possimus amet dolor eaque laborum cumque magnam molestiae earum modi esse quas voluptate eligendi, accusantium dolorem temporibus quisquam! Sit voluptate nam eos vero deserunt magnam fugit magni consequuntur deleniti alias dolorem harum quos corrupti, quae excepturi molestias. Id maxime temporibus ducimus.</p>
+      </div>
+    </div>
+  </div>
+
+</div>
+<?php
+include 'includes/footer.php';
+ ?>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+include 'includes/header.php';
+?>
+<div class="front-banner">
+  <div class="container">
+    <div class="col-2">
+      <div class="left">
+        <h1>Hello there!!</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente hic dolore. Rerum doloremque quae provident deserunt nostrum reprehenderit maiores maxime delectus. Dolorum accusantium necessitatibus saepe enim earum sapiente illum.</p>
+
+      </div>
+      <div class="right">
+
+      </div>
+
+    </div>
+  </div>
+</div> <!-- end front banner -->
+<div class="container">
+  <h1>My gallery</h1>
+  <div class="hinh">
+    <div class="item">
+      <img src="https://i.pinimg.com/236x/0c/3e/57/0c3e57e043fc53b903fef9ffb6bfbc7b.jpg">
+    </div>
+    <div class="item">
+      <img src="https://i.pinimg.com/236x/c6/1d/8f/c61d8ff1a7158b8852debb3a3a3e9293.jpg">
+    </div>
+    <div class="item">
+      <img src="https://i.pinimg.com/236x/6c/7e/41/6c7e4128982a3ee5b39a3c84baad5fc3.jpg">
+    </div>
+    <div class="item">
+      <img src="https://i.pinimg.com/236x/2d/de/9a/2dde9af8114a73700571ec3dcaa77821.jpg">
+    </div>
+    <div class="item">
+      <img src="https://i.pinimg.com/236x/7a/10/45/7a10451f796e59c352ac7b692261b55a.jpg">
+    </div>
+    <div class="item">
+      <img src="https://i.pinimg.com/236x/09/c9/56/09c956a6dd8f2d7dea83626f4a1ecc87.jpg">
+    </div>
+    <div class="item">
+      <img src="https://i.pinimg.com/236x/9f/1c/41/9f1c412b20486f91a86af0b294b3a70a.jpg">
+    </div>
+    <div class="item">
+      <img src="https://i.pinimg.com/236x/3a/14/c6/3a14c6b77d40b507848150d0aa51f360.jpg">
+    </div>
+  </div>
+</div>
+<br>
+
+<?php
+include 'includes/footer.php';
+?>
